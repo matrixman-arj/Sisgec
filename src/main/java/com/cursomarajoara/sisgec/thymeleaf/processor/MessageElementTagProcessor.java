faa1645d2abc63d4b4152d07dc01748accfa427b
decex <decex@decex-ati-mnt05.decex.eb.mil.br>
2018-03-15 17:29:23 -0300

    Inicio do novo elemento de mensage, não finalizado!

---
+++ src/main/java/com/cursomarajoara/sisgec/thymeleaf/processor/MessageElementTagProcessor.java
@@ -0,0 +1,25 @@
+package com.cursomarajoara.sisgec.thymeleaf.processor;
+
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.model.IProcessableElementTag;
+import org.thymeleaf.processor.element.AbstractElementTagProcessor;
+import org.thymeleaf.processor.element.IElementTagStructureHandler;
+import org.thymeleaf.templatemode.TemplateMode;
+
+public class MessageElementTagProcessor extends AbstractElementTagProcessor{
+
+	private static final String NOME_TAG = "message";
+	private static final int PRECEDENCIA = 1000;
+	
+	
+	public MessageElementTagProcessor(String dialectPrefix) {
+		super(TemplateMode.HTML, dialectPrefix, NOME_TAG, true, null, false, PRECEDENCIA );
+		
+	}
+	@Override
+	protected void doProcess(ITemplateContext context, IProcessableElementTag tag, IElementTagStructureHandler structureHandler) {
+		
+		
+	}
+
+}
