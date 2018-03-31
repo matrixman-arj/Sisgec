e89e73421de9838182aae9b7ee1f396f3a701f81
Nill <Nill@Nill-Estudos>
2018-03-31 13:55:59 -0300

    Prototipando pesquisa de cereveja/curso.

--- src/main/java/com/cursomarajoara/sisgec/controller/CursosController.java
+++ src/main/java/com/cursomarajoara/sisgec/controller/CursosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,11 +14,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cursomarajoara.sisgec.enuns.Turno;
 import com.cursomarajoara.sisgec.model.Curso;
+import com.cursomarajoara.sisgec.model.Disciplina;
+import com.cursomarajoara.sisgec.repository.Cursos;
 import com.cursomarajoara.sisgec.repository.Disciplinas;
 import com.cursomarajoara.sisgec.repository.TiposCursos;
 import com.cursomarajoara.sisgec.service.CadastroCursoService;
 
 @Controller
+@RequestMapping("/cursos")
 public class CursosController {
 	
 	@Autowired
@@ -29,9 +33,12 @@ public class CursosController {
 	
 	@Autowired 
 	private CadastroCursoService cadastroCursoService;
+	
+	@Autowired
+	private Cursos cursos;
 
 		
-	@RequestMapping("/cursos/novo")
+	@RequestMapping("/novo")
 	public ModelAndView  novo(Curso curso) {
 		ModelAndView mv = new ModelAndView("curso/CadastroCurso");
 		mv.addObject("turnos", Turno.values());
@@ -40,7 +47,7 @@ public class CursosController {
 		return mv;
 	}
 	
-	@RequestMapping(value = "/cursos/novo", method = RequestMethod.POST )
+	@RequestMapping(value = "/novo", method = RequestMethod.POST )
 	public ModelAndView  cadastrar(@Valid Curso curso, BindingResult result, Model model, RedirectAttributes attributes) {
 		if(result.hasErrors()) {					
 			return novo(curso);
@@ -49,6 +56,16 @@ public class CursosController {
 		cadastroCursoService.salvar(curso);
 		attributes.addFlashAttribute("mensagem", "Curso salvo com sucesso! ");
 		return new ModelAndView("redirect:/cursos/novo");
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("curso/PesquisaCursos");
+		mv.addObject("tiposCursos", tiposCursos.findAll());
+		mv.addObject("turnos", Turno.values());
+		mv.addObject("disciplinas", disciplinas.findAll());
+		mv.addObject("cursos", cursos.findAll());
+		return mv;
 	}
 	
 }
