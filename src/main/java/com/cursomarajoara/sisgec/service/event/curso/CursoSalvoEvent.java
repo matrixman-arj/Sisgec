120db67bd3d4018c44a12750e220bb50b7badb57
decex <decex@decex-ati-mnt05.decex.eb.mil.br>
2018-04-04 16:13:10 -0300

    Iniciando o salvar foto e redução de tamanho.

---
+++ src/main/java/com/cursomarajoara/sisgec/service/event/curso/CursoSalvoEvent.java
@@ -0,0 +1,26 @@
+package com.cursomarajoara.sisgec.service.event.curso;
+
+import org.springframework.util.StringUtils;
+
+import com.cursomarajoara.sisgec.model.Curso;
+
+
+
+public class CursoSalvoEvent {
+	
+	private Curso curso;	
+	
+
+	public CursoSalvoEvent(Curso curso) {		
+		this.curso = curso;
+	}
+
+	public Curso getCurso() {
+		return curso;
+	}
+	
+	public boolean temFoto() {
+		return !StringUtils.isEmpty(curso.getFoto());
+	}
+
+}
