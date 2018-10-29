f4f7c42ba34358447a1e55660690fdc42aef40b6
decex <decex@decex-ati-mnt05.decex.eb.mil.br>
2018-10-29 17:27:32 -0200

    Configurando mascara de telefone com jquery.

--- src/main/resources/static/javascripts/sisgec.js
+++ src/main/resources/static/javascripts/sisgec.js
@@ -18,7 +18,35 @@ Sisgec.MaskMoney = (function(){
 	
 }());
 
+Sisgec.MaskPhoneNumber = (function(){
+	
+	function MaskPhoneNumber(){
+		this.inputPhoneNumber = $('.js-phone-number');
+		
+	}
+	
+	MaskPhoneNumber.prototype.enable = function(){		
+		var maskBehavior = function (val){
+			return val.replace(/\D/g,'').lenght === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+		};
+		
+		var options = {
+			onKeyPress: function(val, e, field, options) {
+				field.mask(maskBehavior.apply({}, arguments), options);
+			}
+		};
+		
+		this.inputPhoneNumber.mask(maskBehavior, options);
+	}
+	
+	return MaskPhoneNumber;
+	
+}());
+
 $(function() {
 	var maskMoney = new Sisgec.MaskMoney();
 	maskMoney.enable();
+	
+	var maskPhoneNumber = new Sisgec.MaskPhoneNumber();
+	maskPhoneNumber.enable();
 }); 
