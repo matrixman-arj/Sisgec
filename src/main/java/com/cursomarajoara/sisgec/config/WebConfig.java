ed18bc0e4e1cab958d0ef1d204404f38b8b42590
Nill <Nill@192.168.0.20>
2019-03-03 23:38:48 -0300

    Input e componente para data.

--- src/main/java/com/cursomarajoara/sisgec/config/WebConfig.java
+++ src/main/java/com/cursomarajoara/sisgec/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.cursomarajoara.sisgec.config;
 
 import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -9,10 +10,13 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
@@ -102,6 +106,10 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		NumberStyleFormatter integerFormatter = new NumberStyleFormatter("#,##0");
 		conversionService.addFormatterForFieldType(Integer.class, integerFormatter);
 		
+		// API de Datas do Java 8
+		DateTimeFormatterRegistrar dateTimeFormatter = new DateTimeFormatterRegistrar();
+		dateTimeFormatter.setDateFormatter(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		dateTimeFormatter.registerFormatters(conversionService);
 		return conversionService;
 	}
 	
@@ -118,5 +126,13 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		GuavaCacheManager cacheManager = new GuavaCacheManager();
 		cacheManager.setCacheBuilder(cacheBuilder);
 		return cacheManager;
-	}		
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource bundle = new ReloadableResourceBundleMessageSource();
+		bundle.setBasename("classpath:/messages");
+		bundle.setDefaultEncoding("UTF-8"); // http://www.utf8-chartable.de/
+		return bundle;
+	}
 }
--- src/main/resources/static/javascripts/sisgec.js
+++ src/main/resources/static/javascripts/sisgec.js
@@ -58,6 +58,25 @@ Sisgec.MaskCepNumber = (function() {
 	
 }());
 
+Sisgec.MaskDate = (function(){
+	
+	function MaskDate() {
+		this.inputDate = $('.js-date');
+	}
+	
+	MaskDate.prototype.enable = function() {
+		this.inputDate.mask('00/00/0000');
+		this.inputDate.datepicker({
+			orientation: 'bottom',
+			language: 'pt-BR',
+			autoclose: true
+		});
+	}
+	
+	return MaskDate;
+	
+}());
+
 $(function() {
 	var maskMoney = new Sisgec.MaskMoney();
 	maskMoney.enable();
@@ -67,4 +86,7 @@ $(function() {
 	
 	var maskCepNumber = new Sisgec.MaskCepNumber();
 	maskCepNumber.enable();
+	
+	var maskDate = new Sisgec.MaskDate();
+	maskDate.enable();
 }); 
