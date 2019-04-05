36d8a8e273d6124064f0edd50e38a874c563ebf8
Administrador <Administrador@decex-ati-mnt05.decex.eb.mil.br>
2019-04-05 11:50:34 -0300

    Tela de login customizado.

--- src/main/resources/db/migration/V09__criar_tabela_curso_disciplina_grade.sql
+++ src/main/resources/db/migration/V09__criar_tabela_curso_disciplina_grade.sql
@@ -1,7 +1,7 @@
 CREATE TABLE grade (
     codigo BIGINT(20) PRIMARY KEY,
     nome VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE curso_disciplina (
     codigo_curso BIGINT(20) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE curso_disciplina (
     PRIMARY KEY (codigo_curso, codigo_disciplina),
     FOREIGN KEY (codigo_curso) REFERENCES curso(codigo_curso),
     FOREIGN KEY (codigo_disciplina) REFERENCES disciplina(codigo_disciplina)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE disciplina_grade (
     codigo_disciplina BIGINT(20) NOT NULL,
@@ -17,4 +17,4 @@ CREATE TABLE disciplina_grade (
     PRIMARY KEY (codigo_disciplina, codigo_grade),
     FOREIGN KEY (codigo_disciplina) REFERENCES disciplina(codigo_disciplina),
     FOREIGN KEY (codigo_grade) REFERENCES grade(codigo)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
