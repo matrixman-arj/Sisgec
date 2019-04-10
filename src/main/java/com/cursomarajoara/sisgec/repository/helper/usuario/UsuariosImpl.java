a42b350e8fd448028d25530a8454ef3cd15d88f9
Administrador <Administrador@decex-ati-mnt05.decex.eb.mil.br>
2019-04-10 16:26:28 -0300

    Autenticando usuário e senha no banco de dados.

--- src/main/java/com/cursomarajoara/sisgec/repository/helper/usuario/UsuariosImpl.java
+++ src/main/java/com/cursomarajoara/sisgec/repository/helper/usuario/UsuariosImpl.java
@@ -1,5 +1,7 @@
 package com.cursomarajoara.sisgec.repository.helper.usuario;
 
+import java.util.Optional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -62,5 +64,13 @@ public class UsuariosImpl implements UsuariosQueries {
 			}			
 			
 		}
+	}
+
+	@Override
+	public Optional<Usuario> porEmailEAtivo(String email) {
+		
+		return manager
+				.createQuery("from Usuario where lower(email) = lower(:email) and ativo = true", Usuario.class)
+				.setParameter("email", email).getResultList().stream().findFirst();
 	}	
 }
--- src/main/java/com/cursomarajoara/sisgec/repository/helper/usuario/UsuariosQueries.java
+++ src/main/java/com/cursomarajoara/sisgec/repository/helper/usuario/UsuariosQueries.java
@@ -1,5 +1,7 @@
 package com.cursomarajoara.sisgec.repository.helper.usuario;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -7,6 +9,8 @@ import com.cursomarajoara.sisgec.model.Usuario;
 import com.cursomarajoara.sisgec.repository.filter.UsuarioFilter;
 
 public interface UsuariosQueries {
+	
+	public Optional<Usuario> porEmailEAtivo(String email);
 	
 	public Page<Usuario> filtrar(UsuarioFilter filtro, Pageable pageable);
 
