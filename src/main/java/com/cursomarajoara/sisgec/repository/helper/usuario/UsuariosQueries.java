e20a37b727978c45dbf7696ca0e75b6c1c4721c6
Nill_Estudos <Nill_Estudos@192.168.2.8>
2019-09-14 01:22:23 -0300

    Trabalhando no filtro por grupo

--- src/main/java/com/cursomarajoara/sisgec/repository/helper/usuario/UsuariosQueries.java
+++ src/main/java/com/cursomarajoara/sisgec/repository/helper/usuario/UsuariosQueries.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import com.cursomarajoara.sisgec.model.Usuario;
+import com.cursomarajoara.sisgec.repository.filter.UsuarioFilter;
 
 public interface UsuariosQueries {
 	
 	public Optional<Usuario> porEmailEAtivo(String email);
 		
 	public List<String> permissoes(Usuario usuario); 
+	
+	public List<Usuario> filtrar(UsuarioFilter filtro);
 
 
 
